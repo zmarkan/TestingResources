761af81c31e2e461717ea609dd968bc66ad43527
zan <zan.markan@gmail.com>
2015-12-25 09:17:40 +0000

    Initial commit

---
+++ app/src/main/java/com/zmarkan/testingresources/ResourcesFeedActivity.java
@@ -0,0 +1,13 @@
+package com.zmarkan.testingresources;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResourcesFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources_feed);
+    }
+}
